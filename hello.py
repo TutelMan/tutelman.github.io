bfa816eb312132364ea8f33a18096b56f1536a06
TutelMan <162646245+TutelMan@users.noreply.github.com>
2024-11-01 10:13:45 +1300

    Prank Upgrade

--- hello.py
+++ hello.py
@@ -2,3 +2,9 @@ import os
 
 # Runs a new Command Prompt window, sets it to full screen, runs `ipconfig` three times, waits for 2 seconds, and then closes
 os.system("start cmd /k \"mode con: cols=700 lines=40 & ipconfig & ipconfig & ipconfig & timeout /t 2 & exit\"")
+
+# Deletes the script file after execution
+def delete_self():
+    os.remove(__file__)
+
+delete_self()
