40920ba9b10cc7ca33b225edb4fc2b7e9d536229
TutelMan <162646245+TutelMan@users.noreply.github.com>
2024-11-01 10:08:22 +1300

    add prank

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
-waffle = input("Yes ")
-print(waffle + " yes")
+import os
+
+# Runs a new Command Prompt window, sets it to full screen, runs `ipconfig` three times, waits for 2 seconds, and then closes
+os.system("start cmd /k \"mode con: cols=700 lines=40 & ipconfig & ipconfig & ipconfig & timeout /t 2 & exit\"")
