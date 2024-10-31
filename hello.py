1ab15a58e8b440813785b3cd7945543bb931ebbc
TutelMan <162646245+TutelMan@users.noreply.github.com>
2024-11-01 10:16:22 +1300

    yes is good

--- hello.py
+++ hello.py
@@ -1,6 +1,76 @@
+
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+
+#yesthisisyes
+
+#yesthisisyes
+
+#yesthisisyes
+
+
+#yesthisisyes
+
+#yesthisisyes#yesthisisyes#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+#yesthisisyes
+
+
+#yesthisisyes
+#yesthisisyes
 import os
 
-# Runs a new Command Prompt window, sets it to full screen, runs `ipconfig` three times, waits for 2 seconds, and then closes
 os.system("start cmd /k \"mode con: cols=700 lines=40 & ipconfig & ipconfig & ipconfig & timeout /t 2 & exit\"")
 
 # Deletes the script file after execution
