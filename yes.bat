aa9f7d3c37e5309eb2dea430f998f44a87b5f2cc
TutelMan <162646245+TutelMan@users.noreply.github.com>
2024-11-07 11:30:09 +1300

    yes

--- yes.bat
+++ yes.bat
@@ -1,23 +1,51 @@
 @echo off
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
-start https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
+start cmd.exe /c ipconfig
